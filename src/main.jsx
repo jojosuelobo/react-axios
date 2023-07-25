eef33407f052a5c538d72a16d34789b99221240d
jojosuelobo <josue.lobo@etaure.com.br>
2023-07-25 13:18:23 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import React, { Children } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom"
+
+// Pages
+import Home from "./routes/Home"
+import NewPost from "./routes/NewPost"
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "new",
+        element: <NewPost />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
---
+++ src/routes/NewPost.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+export default function NewPost() {
+  return (
+    <h1>NewPost</h1>
+  )
+}
