0f85f226c568df75089fa22dd3e84552f779d215
jojosuelobo <josue.lobo@etaure.com.br>
2023-07-25 14:23:01 -0300

    get json placeholder

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,26 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import './Home.css'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default function Home() {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      console.log(response)
+    } catch (err) {
+      console.log(err)
+    } 
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <h1>Home</h1>
   )
